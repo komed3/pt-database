4a47bc56347fe4580b91538822f8474a7b13c48a
komed3 <pk97@outlook.de>
2026-04-18 18:47:22 +0200

    Create unit/time.ts

---
+++ unit/time.ts
@@ -0,0 +1,61 @@
+import type { Quantity } from '@pseinfo/database-schema/abstract/unit';
+import { MetricSystem } from '@pseinfo/database-schema/enum/util';
+
+export default ( {
+  dimension: {
+    symbol: 'T',
+    name: 'time',
+    si: true,
+    vector: [ 0, 0, 1, 0, 0, 0, 0 ]
+  },
+  baseUnit: 's',
+  units: {
+    s: {
+      symbol: 's',
+      name: 'second',
+      system: MetricSystem.METRIC,
+      isBase: true,
+      prefixable: true
+    },
+    min: {
+      symbol: 'min',
+      name: 'minute',
+      system: MetricSystem.METRIC,
+      isBase: false,
+      prefixable: false,
+      conversion: {
+        factor: 60
+      }
+    },
+    h: {
+      symbol: 'h',
+      name: 'hour',
+      system: MetricSystem.METRIC,
+      isBase: false,
+      prefixable: false,
+      conversion: {
+        factor: 3600
+      }
+    },
+    d: {
+      symbol: 'd',
+      name: 'day',
+      system: MetricSystem.METRIC,
+      isBase: false,
+      prefixable: false,
+      conversion: {
+        factor: 86400
+      }
+    },
+    a: {
+      symbol: 'a',
+      name: 'year',
+      system: MetricSystem.METRIC,
+      isBase: false,
+      prefixable: false,
+      conversion: {
+        factor: 31557600
+      }
+    }
+  }
+} ) as const satisfies Quantity< 'time' >;
